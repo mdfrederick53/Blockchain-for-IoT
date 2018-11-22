a7e2de78611da91fa56e877de3ded51d4fa0f9b0
Matthew Frederick <Matt@Matthews-MBP.attlocal.net>
2018-11-22 11:22:17 -0600

    Initial commit

---
+++ SmartAppliance.java
@@ -0,0 +1,14 @@
+/*
+*  SmartAppliance.java
+*
+*  SmartAppliance is any appliance that should not be able to send data to other
+*  devices freely.  This means that they should only send data when prompted by
+*  another device.  SmartAppliances may include lights, security systems,
+*  fridges, and other appliances
+*/
+
+
+public class SmartAppliance extends Smart
+{
+   
+}
